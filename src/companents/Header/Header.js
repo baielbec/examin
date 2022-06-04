5d6fc0c48812f44436f7dd598b0b2d4d14cdc36a
baielbec <asanovbajel303@gmail.com>
2022-06-04 15:18:15 +0300

    item

--- src/companents/Header/Header.js
+++ src/companents/Header/Header.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import Logo from "../../images/image 160 (Traced).svg"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCartShopping} from "@fortawesome/free-solid-svg-icons";
+import {useSelector} from "react-redux";
+
 const Header = () => {
+    const {basket: el} = useSelector(s => s)
+
     return (
-        <div className="bg-[#010049] ">
-            <div className="container">
-                <div className="flex justify-around">
+        <div className="  left-0 right-0 m-0 top-0 bg-[#010049] ">
+            <div className="container mx-auto ">
+                <div className="flex justify-around items-center">
                     <Link to={"/"}
-                          className="py-2 text-white font-medium text-3xl">BOOKShop</Link>
+                          className="py-2 text-white font-mochiy font-bold text-2xl">BOOKShop</Link>
                     <Link to={'/shop-basket'}>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                            <img src={Logo} alt=""/></button>
+                        <button className="flex flex-col  w-full  text-white font-nunito font-bold py-3 px-5">
+                            <p className='flex text-center mx-auto'>{el.length === 0 ? '' : el.length}</p>
+                            <FontAwesomeIcon
+                                className="pl-5 w-6 h-6"
+                                icon={faCartShopping}/>
+                            <span className="flex justify-between">
+                                Корзина
+                            </span>
+                        </button>
                     </Link>
 
                 </div>
